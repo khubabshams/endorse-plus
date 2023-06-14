1346822c4c753e78002ca946e118ace423e0fb63
Khubab <khubab220@gmail.com>
2023-06-14 18:13:14 +0000

    Add route and switch and update styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/boosted" render={() => <h1>Boosted</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/requests" render={() => <h1>Requests</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/profile" render={() => <h1>Profile</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/logout" render={() => <h1>Logout</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/404" render={() => <h1>Page not found 404</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,53 +2,56 @@ import React from 'react'
 import styles from '../styles/NavBar.module.css'
 import { Nav, Navbar, Container, Image } from 'react-bootstrap'
 import logo from '../assets/logo.png'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-            <Navbar.Brand href="#home"><Image src={logo} alt="logo" height="45" /></Navbar.Brand>
+            <NavLink to="/">
+                <Navbar.Brand><Image src={logo} alt="logo" height="45" /></Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav className="ml-auto text-center" navbarScroll>
-                    <Nav.Link href="#home">
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
                         <i class="fa-solid fa-house-chimney"></i>
-                        <br />
+                        <br/>
                         Home
-                    </Nav.Link>
+                    </NavLink>
                     {/* User Authenticated */}
-                    <Nav.Link href="#boosted">
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/boosted">
                         <i class="fa-solid fa-rocket"></i>
-                        <br />
+                        <br/>
                         Boosted
-                    </Nav.Link>
-                    <Nav.Link href="#requests">
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/requests">
                         <i class="fa-solid fa-envelope"></i>
-                        <br />
+                        <br/>
                         Requests
-                    </Nav.Link>
-                    <Nav.Link href="#logout">
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/logout">
                         <i class="fa-solid fa-user-minus"></i>
-                        <br />
+                        <br/>
                         Logout
-                    </Nav.Link>
-                    <Nav.Link href="#profile">
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/profile">
                         <i class="fa-solid fa-user"></i>
-                        <br />
+                        <br/>
                         User Avatar
-                    </Nav.Link>
+                    </NavLink>
                     {/* User Authenticated End */}
 
                     {/* User Not Authenticated */}
-                    <Nav.Link href="#signin">
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
                         <i class="fa-solid fa-user"></i>
-                        <br />
+                        <br/>
                         Sign In
-                    </Nav.Link>
-                    <Nav.Link href="#signup">
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
                         <i class="fa-solid fa-user-plus"></i>
-                        <br />
+                        <br/>
                         Sign Up
-                    </Nav.Link>
+                    </NavLink>
                     {/* User Not Authenticated */}
                 </Nav>
             </Navbar.Collapse>
