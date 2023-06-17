74afcbbd8dbd2cebfe519cb385277b4066ba957a
Khubab <khubab220@gmail.com>
2023-06-17 13:59:25 +0000

    Fix react dom issues by using  createRoot and change class to classname in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,28 +14,28 @@ const NavBar = () => {
             <Navbar.Collapse id="navbarScroll">
                 <Nav className="ml-auto text-center" navbarScroll>
                     <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
-                        <i class="fa-solid fa-house-chimney"></i>
+                        <i className="fa-solid fa-house-chimney"></i>
                         <br/>
                         Home
                     </NavLink>
                     {/* User Authenticated */}
                     <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/boosted">
-                        <i class="fa-solid fa-rocket"></i>
+                        <i className="fa-solid fa-rocket"></i>
                         <br/>
                         Boosted
                     </NavLink>
                     <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/requests">
-                        <i class="fa-solid fa-envelope"></i>
+                        <i className="fa-solid fa-envelope"></i>
                         <br/>
                         Requests
                     </NavLink>
                     <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/logout">
-                        <i class="fa-solid fa-user-minus"></i>
+                        <i className="fa-solid fa-user-minus"></i>
                         <br/>
                         Logout
                     </NavLink>
                     <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/profile">
-                        <i class="fa-solid fa-user"></i>
+                        <i className="fa-solid fa-user"></i>
                         <br/>
                         User Avatar
                     </NavLink>
@@ -43,12 +43,12 @@ const NavBar = () => {
 
                     {/* User Not Authenticated */}
                     <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-                        <i class="fa-solid fa-user"></i>
+                        <i className="fa-solid fa-user"></i>
                         <br/>
                         Sign In
                     </NavLink>
                     <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-                        <i class="fa-solid fa-user-plus"></i>
+                        <i className="fa-solid fa-user-plus"></i>
                         <br/>
                         Sign Up
                     </NavLink>
