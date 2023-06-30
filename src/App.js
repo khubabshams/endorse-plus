b203e188d3b10245f30dacc33f079bc8496f8f0b
Khubab <khubab220@gmail.com>
2023-06-30 17:55:00 +0000

    Add home and boosted routes to app.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,30 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import RecommendationCreateEditForm from "./pages/recommendations/RecommendationCreateEditForm";
 import RecommendationPage from "./pages/recommendations/RecommendationPage";
+import RecommendationsListPage from "./pages/recommendations/RecommendationsListPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
-      <Container className={styles.Main}>
+      <Container className={`${styles.Main}`}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/" render={() => <RecommendationsListPage />} />
+          <Route
+            exact
+            path="/boosted"
+            render={() => (
+              <RecommendationsListPage
+                filter={`boosts__profile=${profile_id}&ordering=-boosts__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/boosted" render={() => <h1>Boosted</h1>} />
           <Route exact path="/requests" render={() => <h1>Requests</h1>} />
           <Route exact path="/profile" render={() => <h1>Profile</h1>} />
           <Route exact path="/logout" render={() => <h1>Logout</h1>} />
