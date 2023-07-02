87d8eb7554db98621c828e34cb6d5a39a23464c5
kshamse <khubab220@gmail.com>
2023-07-02 19:46:44 +0100

    Move the app routes to a separate file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
+import { AppRoutes } from "./AppRoutes";
 import { Container } from "react-bootstrap";
 import styles from "./App.module.css";
-import NavBar from "./components/NavBar";
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 import "./api/axiosDefaults";
-import SignUpForm from "./pages/auth/SignUpForm";
-import SignInForm from "./pages/auth/SignInForm";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
-import RecommendationsListPage from "./pages/recommendations/RecommendationsListPage";
-import RecommendationCreateEditForm from "./components/recommendation/RecommendationCreateEditForm";
-import RecommendationPage from "./pages/recommendations/RecommendationPage";
-import RecommendationRouter from "./pages/recommendations/RecommendationRouter";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -19,38 +13,7 @@ function App() {
     <div className={styles.App}>
       <NavBar />
       <Container className={`${styles.Main}`}>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => <RecommendationRouter profile_id={profile_id} />}
-          />
-          <Route
-            exact
-            path="/boosted"
-            render={() => (
-              <RecommendationsListPage
-                filter={`boosts__profile=${profile_id}&ordering=-boosts__created_at&`}
-              />
-            )}
-          />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/requests" render={() => <h1>Requests</h1>} />
-          <Route exact path="/profile" render={() => <h1>Profile</h1>} />
-          <Route exact path="/logout" render={() => <h1>Logout</h1>} />
-          <Route
-            exact
-            path="/recommendations/create/:receiver_id"
-            render={() => <RecommendationCreateEditForm />}
-          />
-          <Route
-            exact
-            path="/recommendations/:id"
-            render={() => <RecommendationPage />}
-          />
-          <Route render={() => <h1>Page not found 404</h1>} />
-        </Switch>
+        <AppRoutes profile_id={profile_id} />
       </Container>
     </div>
   );
---
+++ src/AppRoutes.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import RecommendationsListPage from "./pages/recommendations/RecommendationsListPage";
+import RecommendationCreateEditForm from "./components/recommendation/RecommendationCreateEditForm";
+import RecommendationPage from "./pages/recommendations/RecommendationPage";
+import RecommendationRouter from "./pages/recommendations/RecommendationRouter";
+import RequestCreateForm from "./components/requests/RequestCreateForm";
+import NotFoundPage from "./pages/NotFoundPage";
+
+export function AppRoutes({ profile_id }) {
+  return (
+    <Switch>
+      {/* General Routes */}
+      <Route exact path="/signin" render={() => <SignInForm />} />
+      <Route exact path="/signup" render={() => <SignUpForm />} />
+      <Route exact path="/requests" render={() => <h1>Requests</h1>} />
+      <Route exact path="/profile" render={() => <h1>Profile</h1>} />
+      <Route exact path="/logout" render={() => <h1>Logout</h1>} />
+
+      {/* Recommendations Routes */}
+      <Route
+        exact
+        path="/recommendations/create/:receiver_id"
+        render={() => <RecommendationCreateEditForm />}
+      />
+      <Route
+        exact
+        path="/recommendations/:id"
+        render={() => <RecommendationPage />}
+      />
+      <Route
+        exact
+        path="/"
+        render={() => <RecommendationRouter profile_id={profile_id} />}
+      />
+      <Route
+        exact
+        path="/boosted"
+        render={() => (
+          <RecommendationsListPage
+            filter={`boosts__profile=${profile_id}&ordering=-boosts__created_at&`}
+          />
+        )}
+      />
+
+      {/* Requests Routes */}
+      <Route
+        exact
+        path="/requests/create/:receiver_id"
+        render={() => <RequestCreateForm />}
+      />
+
+      {/* 404 Route */}
+      <Route render={() => <NotFoundPage />} />
+    </Switch>
+  );
+}
