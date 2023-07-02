bb40f4c78a9db6df046660af76bed004e51cb5c4
kshamse <khubab220@gmail.com>
2023-07-02 01:55:39 +0100

    Add top endorsed profiles and attach it to router view

--- src/pages/recommendations/RecommendationRouter.js
+++ src/pages/recommendations/RecommendationRouter.js
@@ -1,49 +1,57 @@
 import React from "react";
 import { Col, Row, Tab, Nav } from "react-bootstrap";
 import RecommendationsListPage from "./RecommendationsListPage";
+import TopEndorsedProfiles from "../../components/TopEndorsedProfiles";
 
 const RecommendationRouter = ({ profile_id }) => {
   return (
-    <>
-      {profile_id ? (
-        <Tab.Container defaultActiveKey="received">
-          <Row>
-            <Col xs={12} md={10}>
-              <Nav variant="tabs" fill className="bg-white mb-2">
-                <Nav.Item>
-                  <Nav.Link eventKey="received">
-                    <i className="fa-solid fa-download"></i> Received
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="sent">
-                    <i className="fa-solid fa-upload"></i> Sent
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={10}>
-              <Tab.Content>
-                <Tab.Pane eventKey="received">
-                  <RecommendationsListPage
-                    filter={`receiver=${profile_id}&ordering=-is_featured&`}
-                  />
-                </Tab.Pane>
-                <Tab.Pane eventKey="sent">
-                  <RecommendationsListPage
-                    filter={`profile=${profile_id}&ordering=-created_at&`}
-                  />
-                </Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-      ) : (
-        <RecommendationsListPage filter={`ordering=-created_at&`} />
-      )}
-    </>
+    <Row>
+      <Col xs={12} md={9}>
+        <TopEndorsedProfiles  mobile/>
+        {profile_id ? (
+          <Tab.Container defaultActiveKey="received">
+            <Row>
+              <Col>
+                <Nav variant="tabs" fill className="bg-white mb-2">
+                  <Nav.Item>
+                    <Nav.Link eventKey="received">
+                      <i className="fa-solid fa-download"></i> Received
+                    </Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="sent">
+                      <i className="fa-solid fa-upload"></i> Sent
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Tab.Content>
+                  <Tab.Pane eventKey="received">
+                    <RecommendationsListPage
+                      filter={`receiver=${profile_id}&ordering=-is_featured&`}
+                    />
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="sent">
+                    <RecommendationsListPage
+                      filter={`profile=${profile_id}&ordering=-created_at&`}
+                    />
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        ) : (
+          <RecommendationsListPage filter={`ordering=-created_at&`} />
+        )}
+      </Col>
+
+      <Col md={3} className="d-none d-md-block">
+        <TopEndorsedProfiles />
+      </Col>
+    </Row>
   );
 };
 
