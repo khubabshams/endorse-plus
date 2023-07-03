e133967352dd2e2e03e36054ad4a3bd27daa6a08
kshamse <khubab220@gmail.com>
2023-07-03 02:03:34 +0100

    Add request page

--- src/pages/requests/RequestPage.js
+++ src/pages/requests/RequestPage.js
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import RecommendationRequest from "../../components/requests/RecommendationRequest";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const RequestPage = () => {
   const { id } = useParams();
   const [request, setRequests] = useState({ results: [] });
+  const history = useHistory();
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
     const handleMount = async () => {
       try {
         const { data: request } = await axiosReq(`/requests/${id}`);
+        if (
+          ![request?.profile, request?.receiver].includes(
+            currentUser?.profile_id
+          )
+        ) {
+          history.push("/un-authorised");
+        }
         setRequests({ results: [request] });
       } catch (err) {
-        console.log(err);
+        if (err.response?.status === 404) {
+          history.push("/not-found");
+        }
       }
     };
 
@@ -22,7 +34,10 @@ const RequestPage = () => {
 
   return (
     <>
-      <RecommendationRequest {...request.results[0]} setRequests={setRequests} />
+      <RecommendationRequest
+        {...request.results[0]}
+        setRequests={setRequests}
+      />
     </>
   );
 };
