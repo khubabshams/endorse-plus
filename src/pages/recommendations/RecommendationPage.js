2f82580d07357bffd3ea55f184733661ca51871f
kshamse <khubab220@gmail.com>
2023-07-03 02:03:56 +0100

    Handle 404 requests in recommendation page

--- src/pages/recommendations/RecommendationPage.js
+++ src/pages/recommendations/RecommendationPage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Recommendation from "../../components/recommendation/Recommendation";
 
 const RecommendationPage = () => {
   const { id } = useParams();
   const [recommendation, setRecommendations] = useState({ results: [] });
+  const history = useHistory();
 
   useEffect(() => {
     const handleMount = async () => {
@@ -15,7 +16,9 @@ const RecommendationPage = () => {
         );
         setRecommendations({ results: [recommendation] });
       } catch (err) {
-        console.log(err);
+        if (err.response?.status === 404) {
+          history.push("/not-found");
+        }
       }
     };
 
