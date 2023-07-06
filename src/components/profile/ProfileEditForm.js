782b3253960f0ca15e302d6ca774f081db29e572
kshamse <khubab220@gmail.com>
2023-07-06 23:14:39 +0100

    Add edit profile form and Fix image submit issue

---
+++ src/components/profile/ProfileEditForm.js
@@ -0,0 +1,185 @@
+import { React, useState, useRef } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { Button, Figure, Form, Image } from "react-bootstrap";
+import btnStyles from "../../styles/Button.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+import CustomAlert from "../CustomAlert";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
+
+const ProfileEditForm = ({ profile, setEditMode }) => {
+  const [profileData, setProfileData] = useState({
+    name: profile.name,
+    title: profile.title,
+    image: profile.image,
+    summary: profile.summary,
+    linkedin_profile_url: profile?.linkedin_profile_url,
+  });
+  const setCurrentUser = useSetCurrentUser();
+  const setCurrentProfileData = useSetProfileData();
+
+  const [errors, setErrors] = useState({});
+  const { name, title, image, summary, linkedin_profile_url } = profileData;
+  const { id } = useParams();
+  const imageFile = useRef();
+
+  const handleChange = (event) => {
+    setProfileData({
+      ...profileData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    name && formData.append("name", name);
+    title && formData.append("title", title);
+    imageFile?.current?.files[0] &&
+      formData.append("image", imageFile?.current?.files[0]);
+    summary && formData.append("summary", summary);
+    linkedin_profile_url &&
+      formData.append("linkedin_profile_url", linkedin_profile_url);
+
+    try {
+      const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
+
+      onSubmitSuccess(data);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
+
+  const onSubmitSuccess = (data) => {
+    setCurrentUser((currentUser) => ({
+      ...currentUser,
+      profile_image: data.image,
+    }));
+
+    setCurrentProfileData((prevState) => ({
+      ...prevState,
+      pageProfile: {
+        results: [
+          {
+            ...{
+              name: data.name,
+              title: data.title,
+              image: data.image,
+              summary: data.summary,
+              linkedin_profile_url: data.linkedin_profile_url,
+            },
+          },
+        ],
+      },
+    }));
+
+    setEditMode(false);
+  };
+
+  return (
+    <div>
+      <div className="d-flex justify-content-center">
+        {image && (
+          <Figure>
+            <Image
+              className="border-1"
+              src={image}
+              fluid
+              height="100"
+              width="100"
+              roundedCircle
+            />
+          </Figure>
+        )}
+        <>
+          <Button
+            onClick={() => setEditMode(false)}
+            className={`fa-solid fa-undo align-self-start ${btnStyles.Button} ${btnStyles.Option}`}
+          ></Button>
+          <Button
+            form="profileEditForm"
+            type="submit"
+            className={`fa-solid fa-floppy-disk align-self-start ${btnStyles.Button} ${btnStyles.Option}`}
+          ></Button>
+        </>
+      </div>
+
+      <Form onSubmit={handleSubmit} id="profileEditForm">
+        <div className="d-flex justify-content-center">
+          <Form.Group>
+            <Form.Label className="d-none">Image</Form.Label>
+            <Form.File
+              id="image-upload"
+              ref={imageFile}
+              accept="image/*"
+              onChange={(e) => {
+                if (e.target.files.length) {
+                  setProfileData({
+                    ...profileData,
+                    image: URL.createObjectURL(e.target.files[0]),
+                  });
+                }
+              }}
+            />
+          </Form.Group>
+          <CustomAlert error={errors?.image} />
+        </div>
+
+        <Form.Group controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <CustomAlert error={errors?.name} />
+
+        <Form.Group controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <CustomAlert error={errors?.title} />
+
+        <Form.Group controlId="linkedin_profile_url">
+          <Form.Label>linkedin Profile</Form.Label>
+          <Form.Control
+            type="text"
+            name="linkedin_profile_url"
+            value={linkedin_profile_url}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <CustomAlert error={errors?.linkedin_profile_url} />
+
+        <Form.Group controlId="summary">
+          <Form.Label>Summary</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="summary"
+            value={summary}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <CustomAlert error={errors?.summary} />
+
+        <CustomAlert error={errors?.non_field_errors} />
+        <CustomAlert error={errors?.detail} detail={true} />
+      </Form>
+    </div>
+  );
+};
+
+export default ProfileEditForm;
--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -1,20 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 import appStyles from "../../App.module.css";
-import btnStyles from "../../styles/Button.module.css";
 import styles from "../../styles/Profile.module.css";
-import { Button, Card, Image } from "react-bootstrap";
+import { Container, Card, Image } from "react-bootstrap";
+import ProfileEditForm from "./ProfileEditForm";
 
-export function ProfileHeader({ profile }) {
-  return (
+export function ProfileHeader({ profile, is_owner }) {
+  const [editMode, setEditMode] = useState(false);
+
+  const handleEdit = (event) => {
+    event.preventDefault();
+    if (is_owner) {
+      setEditMode(true);
+    }
+  };
+
+  return !editMode ? (
     <Card className="p-4 d-flex flex-column justify-content-center align-items-center">
-      <Image
-        className="border-1"
-        src={profile?.image}
-        height="100"
-        width="100"
-        roundedCircle
-      />
+      <div className="d-flex">
+        <Image
+          className="border-1"
+          src={profile?.image}
+          height="100"
+          width="100"
+          roundedCircle
+        />
+        <span
+          variant="secondary"
+          onClick={handleEdit}
+          className={`fa-solid fa-pen-to-square align-self-start`}
+        ></span>
+      </div>
       <span className={`mt-2 ${styles.Name}`}>{profile?.name}</span>
       <span className="text-muted">{profile?.title}</span>
       <div
@@ -33,16 +49,16 @@ export function ProfileHeader({ profile }) {
           <span> Boosts</span>
         </span>
       </div>
-      <div className="d-flex mt-2">
-        <Button className={`${btnStyles.Button}`}>Edit Profile</Button>
-      </div>
       <div className={`${appStyles.Content} mt-2`}>
         <span>{profile?.summary}</span>
       </div>
       {profile?.linkedin_profile_url && (
         <div className="gap-3 mt-2 d-flex flex-row justify-content-center align-items-center">
           <span>
-            <Link to={profile?.linkedin_profile_url}>
+            <Link
+              to={{ pathname: `${profile?.linkedin_profile_url}` }}
+              target="_blank"
+            >
               <i className="fa fa-linkedin"></i>
             </Link>
           </span>
@@ -54,5 +70,9 @@ export function ProfileHeader({ profile }) {
         </span>
       </div>
     </Card>
+  ) : (
+    <Container className={`bg-white pt-3`}>
+      <ProfileEditForm profile={profile} setEditMode={setEditMode} />
+    </Container>
   );
 }
