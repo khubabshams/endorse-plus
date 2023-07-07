13bd1e08428da0bfdb976edb2000f1e406db55d2
kshamse <khubab220@gmail.com>
2023-07-08 00:57:57 +0100

    Change ordering of top endorsed to be based on received recommendations

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -20,7 +20,7 @@ export const ProfileDataProvider = ({ children }) => {
     const handleMount = async () => {
       try {
         const { data } = await axiosReq.get(
-          `/profiles/?ordering=-boosts_count&`
+          `/profiles/?ordering=-recommendations_received_count&`
         );
         setProfileData((prevProfileData) => ({
           ...prevProfileData,
